58a8101eba7d61c0b38bc38d8024e206e9b720f9
Emiliano Delfino <UP200173@alumnos.upa.edu.mx>
2024-03-22 11:56:37 -0600

    fix issues

--- src/Components/Materiales/Material1.tsx
+++ src/Components/Materiales/Material1.tsx
@@ -1,10 +1,7 @@
 import React from "react";
-import PropTypes from "prop-types"; // Importa PropTypes para validar las props
-import MemoryFoamImg from "../../images/material1_memoryfoam.jpg";
+import MemoryFoamImg from "../../images/material1_memoryfoam.jpg"
 
-function Material1(props) {
-  const { materialName, materialDescription, imageUrl } = props;
-
+function Material1() {
   return (
     <div>
       <div className="container mt-4 mb-4 grid shadow p-3 mb-5 bg-body-tertiary rounded text-center">
@@ -12,7 +9,7 @@ function Material1(props) {
           <div className="col-md-5">
             <img
               className="card-img-top"
-              src={imageUrl}
+              src={MemoryFoamImg}
               alt="Imagen del material"
               style={{ objectFit: 'cover', width: '100%', height: '100%' }}
             />
@@ -20,12 +17,25 @@ function Material1(props) {
           <div className="col-md-7">
             <div className="row">
               <div className="col p-4">
-                <h4>{materialName}</h4>
+                <h4>Nombre del material</h4>
               </div>
             </div>
             <div className="row">
               <div className="col">
-                <p>{materialDescription}</p>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit,
+                  architecto quod? Recusandae voluptates tempore architecto
+                  impedit minima omnis earum eligendi consequuntur mollitia
+                  voluptate quibusdam explicabo ea inventore, reiciendis
+                  consequatur? Nemo.
+                </p>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Tempora consequatur deleniti consequuntur aperiam ipsa, modi
+                  officia placeat exercitationem voluptatum quasi. Suscipit
+                  cupiditate consectetur accusantium, sint quaerat modi quia
+                  quis atque.
+                </p>
               </div>
             </div>
           </div>
@@ -34,12 +44,5 @@ function Material1(props) {
     </div>
   );
 }
-
-// Define las propiedades esperadas y sus tipos utilizando PropTypes
-Material.propTypes = {
-  materialName: PropTypes.string.isRequired,
-  materialDescription: PropTypes.string.isRequired,
-  imageUrl: PropTypes.string.isRequired
-};
 
 export default Material1;
